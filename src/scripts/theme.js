02aa2ffec0107772a5efe8bdba12023c1294021f
Fathursyah Awandanu <fathur.syah1@gmail.com>
2025-03-08 05:11:02 +0700

    update : dark mode fouc fix

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,8 +1,6 @@
-if (localStorage.getItem("theme") === null) {
-    localStorage.setItem("theme", "light");
-}
-if (localStorage.getItem("theme") === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
-    document.documentElement.classList.add("dark");
+if (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches || localStorage.theme === 'dark') {
+    document.documentElement.dataset.theme = 'dark'
 } else {
-    document.documentElement.classList.remove("dark");
+    document.documentElement.dataset.theme = 'light'
+    
 }
