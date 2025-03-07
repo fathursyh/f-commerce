6aed7aa2b9016032f22bc22ba1b4b5dea61624bd
fathursyh <fathur.syah1@gmail.com>
2025-03-08 03:54:42 +0700

    update : fix fetch product when null

---
+++ src/scripts/theme.js
@@ -0,0 +1,8 @@
+if (localStorage.getItem("theme") === null) {
+    localStorage.setItem("theme", "light");
+}
+if (localStorage.getItem("theme") === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+    document.documentElement.classList.add("dark");
+} else {
+    document.documentElement.classList.remove("dark");
+}
